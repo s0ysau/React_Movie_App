9ea8e0668d59c1c61ba704201388bbd3e0e35bcc
jerricklc@gmail.com <jerricklc@gmail.com>
2023-09-12 09:59:05 -0700

    remove black background

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,13 @@ import Logo from "./Logo";
 
 export default function Header ({getMovie}) {
   return (
-    <section>
-      <section className="flex p-2 bg-black">
+    <section className="flex justify-center p-2 bg-black">
         <Logo/>
         <Form movieSearch={getMovie} />
         {/* <section className="flex">
           <p className="text-white">Sign In</p> 
           <p className="text-white">Log In</p>
         </section> */}
-      </section>
     </section>
   )
 }
