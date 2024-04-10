37bde1ff3d53dc612545f93a497799f52e0eea48
jerricklc@gmail.com <jerricklc@gmail.com>
2024-04-10 13:58:50 -0700

    added require('dotenv').config() to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
+require('dotenv').config()
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
